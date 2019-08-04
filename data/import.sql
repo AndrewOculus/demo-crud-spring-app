6887ee4d011d2770bf76eaf23820fdc009d7a4db
Andrew <andrewoculus@gmail.com>
2019-08-04 20:47:06 +0300

    add items adding form

---
+++ data/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO book (id, name, brand, maidin, price) VALUES (
+1, "some name" , "some brand", "maidin", "120.5" );
--- src/main/java/com/middlewareocean/democrudapp/AppController.java
+++ src/main/java/com/middlewareocean/democrudapp/AppController.java
@@ -1,14 +1,20 @@
 package com.middlewareocean.democrudapp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
 @Controller
 public class AppController {
+    Logger logger = LoggerFactory.getLogger(AppController.class);
+
     @Autowired
     private ProductService productService;
 
@@ -17,6 +23,19 @@ public class AppController {
         List<Product> products = productService.listAll();
         model.addAttribute("listProducts", products);
         return "index";
+    }
+
+    @RequestMapping("/new")
+    public String showNewProductForm(Model model){
+        Product product = new Product();
+        model.addAttribute( "product", product);
+        return "new_product";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String saveProduct(@ModelAttribute("product") Product product){
+        productService.save(product);
+        return "redirect:/";
     }
 
 }
