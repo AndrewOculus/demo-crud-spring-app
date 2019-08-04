9c62132c531e10a7d26df3678e3b580363222d3f
Andrew <andrewoculus@gmail.com>
2019-08-04 14:44:36 +0300

    first commit

---
+++ src/main/java/com/middlewareocean/democrudapp/DemocrudappApplication.java
@@ -0,0 +1,13 @@
+package com.middlewareocean.democrudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocrudappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocrudappApplication.class, args);
+	}
+
+}
